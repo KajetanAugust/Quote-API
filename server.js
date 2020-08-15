257e6ab0354f2ab4eecd8deeec5560c914fde3f3
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2020-08-15 17:16:13 +0200

    Added code for GET method, fetching quote by author not working for now

--- server.js
+++ server.js
@@ -6,13 +6,26 @@ const { getRandomElement } = require('./utils');
 
 const PORT = process.env.PORT || 4001;
 
-app.use('/api', express.static('public'));
-
-app.get('/quotes/random', (req, res, next)  => {
-
-})
+app.use( express.static('public'));
 
 
+app.get('/api/quotes/', (req,res,next) => {
+    if(!req.query.person) {
+        res.send({quotes});
+    } else {
+       const person = req.query.person.toLowerCase();
+        let newQuotes = quotes.filter(quote => {
+            return quote.person.toLowerCase() == person;
+        });
+            res.send({newQuotes});
+    }
+
+});
+
+app.get('/api/quotes/random/', (req, res, next)  => {
+    let randomQuote = getRandomElement(quotes);
+    res.send(randomQuote);
+});
 
 
 app.listen(PORT, () => {
